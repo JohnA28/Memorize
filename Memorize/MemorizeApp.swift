ca6247d9da6e63262df36b540ef4d1ebe842d1db
JohnA28 <John.Alban41@gmail.com>
2023-02-19 03:36:06 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by John Alban on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
