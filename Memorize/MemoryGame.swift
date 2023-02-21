832c7cc4cab092f6ab4acf123a30164402305427
JohnA28 <John.Alban41@gmail.com>
2023-02-20 20:53:25 -0500

    Made Card struct identifiable for use in ContentView

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,7 +10,7 @@ import Foundation
 struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
-    func choose (_: Card){
+    func choose (_ card: Card){
         
     }
     
@@ -19,14 +19,15 @@ struct MemoryGame<CardContent> {
         // add numberOfPairsOfCards x 2 cards to create array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
     
-    struct Card {
+    struct Card: Identifiable {
         var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
